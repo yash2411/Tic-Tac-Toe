a9730b95f7bd38f6cbb1067403640b16af09a479
Yash <yashpandey1106@gmail.com>
2021-12-18 11:49:01 +0530

    Handled form submission and player data

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,20 @@
 const playerConfigOverlay=document.getElementById('config-overlay');
 const backdropElement=document.getElementById('backdrop')
 
+let editedPlayer=0;
+
+const players=[
+    {name: '', symbol: 'X'},
+    {name: '', symbol: 'O'}
+]
+
 const editPlayer1BtnElement = document.getElementById('edit-player1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player2-btn');
 
 const cancelBtnElement=document.getElementById('cancel-btn');
 
+const formElement=document.querySelector('form');
+const errorsOutputElement=document.getElementById('config-errors')
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -13,3 +22,4 @@ editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 cancelBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
+formElement.addEventListener('submit', savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,6 @@
-function openPlayerConfig()
+function openPlayerConfig(event)
 {
+    editedPlayer= +event.target.dataset.playerid;
     playerConfigOverlay.style.display='block';
     backdropElement.style.display='block';
 }
@@ -8,4 +9,26 @@ function closePlayerConfig()
 {
     playerConfigOverlay.style.display='none';
     backdropElement.style.display='none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent='';
+    formElement.firstElementChild.lastElementChild.value='';
+}
+
+function savePlayerConfig(event)
+{
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayerName=formData.get('playername').trim();
+
+    if(!enteredPlayerName)
+    {
+        event.target.firstElementChild.classList.add('error')
+        errorsOutputElement.textContent='Please Enter a Valid Name';
+        return;
+    }
+
+    const updatePlayerElement=document.getElementById('player-'+editedPlayer+'-data');
+    updatePlayerElement.children[1].textContent=enteredPlayerName;
+    players[editedPlayer-1].name=enteredPlayerName;
+    closePlayerConfig();
 }
